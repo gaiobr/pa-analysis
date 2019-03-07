4d0e017a6412654b3d42d6bfa510eba4331cbd53
gaiobr <jhonguedes@gmail.com>
2019-03-06 22:41:55 -0300

    Was started improvements to create a PT dataset for page creations and to calculate means
    Some errors are detected:
    - CNUC Dataset has some inconsistencies, like PA that have a PT Page, but is not listed in the Dataset
    - It was started a script to list these PAs, but another inconsistency was verified: one PA has a link to another PA
    - This problem will be verified in the future

--- explorePageViewsData.R
+++ explorePageViewsData.R
@@ -17,9 +17,9 @@ library(lubridate)
 
 # Load datasets
 cnuc_data <- read_csv("./data/BrazilianProtectedAreas_2018-12-10_reviewed.csv")
-page_creation_data_eng <- read_csv("./data/BPA_WikipediaPages_eng_2019-03-05.csv")
+page_creation_data_eng <- read_csv("./data/BPA_WikipediaPages_eng_2019-03-06.csv")
 
-# Create a Mean dataset
+# Create a Mean Eng dataset
 mean_eng_pageviews <- data.table(
   cod_cnuc = character(),
   mean = numeric(),
@@ -30,7 +30,7 @@ mean_eng_pageviews <- data.table(
 # Load list of CSV files
 files <- list.files("./data/eng/")
 
-# ---- Calculate mean values for all pageviews ----
+# ---- MEAN ENG PAGEVIEWS: Calculate mean values for all ENG pageviews ----
 for (i in 1:length(files)) {
   pa_pageviews <- read_csv(paste0("./data/eng/",files[i]), col_types = cols()) # col_types = cols() supress output information when reading CSV
 
@@ -86,7 +86,7 @@ for (i in 1:length(files)) {
 }
 
 
-# ---- Calculate mean values by month to each CSV file ----
+# ---- MEAN MONTH ENG PAGEVIEWS: Calculate mean values by month to ENG pageviews ----
 # Create a primary table with a row for each month starting from 2015-07
 month_mean_eng_pageviews <- data.table(
   month = seq(
@@ -154,5 +154,114 @@ for (i in 1:length(files)) {
   # Populate data table
   month_mean_eng_pageviews <- merge(x = month_mean_eng_pageviews, y = month_eng_means, by = "month", all = TRUE)
   
+}
+
+
+
+# ---- MEAN PT PAGEVIEWS: Calculate mean values for all PT pageviews ----
+files_pt <- list.files("./data/pt/")
+page_creation_data_pt <- read_csv("./data/BPA_WikipediaPages_pt_2019-03-06.csv")
+
+# Create a Mean Pt dataset
+mean_pt_pageviews <- data.table(
+  cod_cnuc = character(),
+  mean = numeric(),
+  na_rows = integer(),
+  total_days = integer()
+)
+
+for (i in 1:length(files)) {
+  pa_pageviews <- read_csv(paste0("./data/eng/",files[i]), col_types = cols()) # col_types = cols() supress output information when reading CSV
+  
+  # Extract CNUC code from CSV filename
+  cod_cnuc_pageview <- substr(files[i], 0, 12)
+  
+  # Extract page creation 
+  page_creation <- page_creation_data_pt %>%
+    filter(codcnuc == cod_cnuc_pageview) %>%
+    select(page_creation)
+  
+  # Test if pt language exists in CSV Eng File
+  pt_verify <- pa_pageviews %>%
+    subset(pa_pageviews$Language == "pt")
+  
+  if (length(pt_verify$Language) > 0) {
+    # Rearrange CSV Pageviews Dataset
+    pa_pageviews <- pa_pageviews %>%
+      filter(Language == "pt") %>%
+      select(4:ncol(pa_pageviews)) %>%
+      t()
+    
+    pa_pageviews <- as.data.frame(pa_pageviews)
+    
+    pa_pageviews <- cbind(rownames(pa_pageviews), pa_pageviews)
+    colnames(pa_pageviews) <- c("Dates", "Views")
+    
+    print(cod_cnuc_pageview)
+
+    # Define start row based in the date of page creation (after 2015-07-01, row will be greater than 1)
+    if (page_creation$page_creation >= as.Date("2015-07-01")) {
+      row_num <- which(as.Date(pa_pageviews$Dates) == page_creation$page_creation)
+    } else {
+      row_num <- which(as.Date(pa_pageviews$Dates) == as.Date("2015-07-01"))
+    }
+    
+    print(nrow(pa_pageviews))
+    
+    # Select a subset when necessary
+    pa_pageviews <- pa_pageviews %>%
+      slice(row_num:nrow(pa_pageviews))
+    
+    # Calculate mean
+    pa_mean <- mean(as.integer(pa_pageviews$Views), na.rm = TRUE)
+    print(pa_mean)
+    
+    # Count how many rows contains data filled with NA
+    na_rows <- pa_pageviews %>%
+      filter(is.na(Views)) %>%
+      count()
+    
+    na_rows <- as.integer(na_rows)
+    
+    # Add information to the dataset
+    new_row <- data.table(cod_cnuc = cod_cnuc_pageview, mean = pa_mean, na_rows = na_rows, total_days = nrow(pa_pageviews))
+    
+    mean_pt_pageviews <- rbind(mean_pt_pageviews, new_row)  
+  }
+  
+
+  
+}
+
+
+# ---- IMPORTANT TO VERIFY ----
+# Some PAs that have PT data, but is not listed in the CNUC Dataset
+# It is important to make a comparison between CNUC Name and WikiNAme (engs and portuguese)
+# For example: Floresta Nacional de Brasília (0000.00.0086) está vinculada a uma PA que não é a correta
+for (i in 1:length(files)) {
+  pa_pageviews <- read_csv(paste0("./data/eng/",files[i]), col_types = cols()) # col_types = cols() supress output information when reading CSV
+  
+  # Extract CNUC code from CSV filename
+  cod_cnuc_pageview <- substr(files[i], 0, 12)
+  
+  # Extract page creation 
+  page_creation <- page_creation_data_pt %>%
+    filter(codcnuc == cod_cnuc_pageview) %>%
+    select(page_creation)
+  
+  # Test if pt language exists in CSV Eng File
+  pt_verify <- pa_pageviews %>%
+    subset(pa_pageviews$Language == "pt")
+  
+  if (length(pt_verify$Language) > 0) {
+    verify_pgcreation <- page_creation_data_pt %>%
+      subset(page_creation_data_pt$codcnuc == cod_cnuc_pageview)
+    
+    if (length(verify_pgcreation$page_creation) < 1) {
+      print(cod_cnuc_pageview)
+    }
+    
+  }
+
 }
 
--- getWikiPageCreation.R
+++ getWikiPageCreation.R
@@ -17,24 +17,79 @@ library(data.table)
 # Read PA Dataset
 pa_original <- read_csv("./data/BrazilianProtectedAreas_2018-12-10_reviewed.csv")
 
-# Create PA dataset with date of page creation
+# ---- Create PA dataset with date of page creation ----
 pa_dataset_eng <- data.table(codcnuc = character(), pa_name = character(), pa_start_views = character(), pa_end_views = character(), page_creation = character())
 
 pa_dataset_pt <- data.table(codcnuc = character(), pa_name = character(), pa_start_views = character(), pa_end_views = character(), page_creation = character())
 
+# ---- Read CSV Eng Files and extract information from filenames ----
+pa_eng_files <- list.files("./data/eng/")
 
-# ---- English ----
+pa_info_eng <- data.table(
+  codcnuc = character(), 
+  pa_start_views = character(), 
+  pa_end_views = character()
+)
 
-# Load PA CSV ENG Files list
-pa_csv_files <- list.files("./data/eng/")
+for (i in 1:length(pa_eng_files)) {
+  # Split Filename
+  
+  pa_filename <- pa_eng_files[i]
+  
+  pa_properties <- unlist(strsplit(pa_filename, "_"))
+  
+  pa_cod_cnuc <- pa_properties[1]
+  pa_start_views <- pa_properties[3]
+  pa_end_views <- unlist(strsplit(pa_properties[4], "[.]"))[1]
+  
+  # Add information to the dataset
+  new_row <- data.table(
+    codcnuc = pa_cod_cnuc, 
+    pa_start_views = pa_start_views, 
+    pa_end_views = pa_end_views
+  )
+  
+  pa_info_eng <- rbind(pa_info_eng, new_row)
+}
 
-# Read all CSV ENG names and get page creation from Wikipedia
-for (i in 1:length(pa_csv_files)) {
-  print(paste0(i, " in ", length(pa_csv_files)))
+# ---- Read CSV Pt Files and extract information from filenames ----
+pa_pt_files <- list.files("./data/pt/")
+
+pa_info_pt <- data.table(
+  codcnuc = character(), 
+  pa_start_views = character(), 
+  pa_end_views = character()
+)
+
+for (i in 1:length(pa_pt_files)) {
+  # Split Filename
+  
+  pa_filename <- pa_pt_files[i]
+  
+  pa_properties <- unlist(strsplit(pa_filename, "_"))
+  
+  pa_cod_cnuc <- pa_properties[1]
+  pa_start_views <- pa_properties[3]
+  pa_end_views <- unlist(strsplit(pa_properties[4], "[.]"))[1]
+  
+  # Add information to the dataset
+  new_row <- data.table(
+    codcnuc = pa_cod_cnuc, 
+    pa_start_views = pa_start_views, 
+    pa_end_views = pa_end_views
+  )
+  
+  pa_info_pt <- rbind(pa_info_pt, new_row)
+}
+
+
+# ----ENG PAGE CREATION: Read all CSV ENG names and get page creation from Wikipedia ----
+for (i in 1:length(pa_eng_files)) {
+  print(paste0(i, " in ", length(pa_eng_files)))
 
   # Split Filename
 
-  pa_filename <- pa_csv_files[i]
+  pa_filename <- pa_eng_files[i]
    
   pa_properties <- unlist(strsplit(pa_filename, "_"))
   
@@ -83,34 +138,76 @@ for (i in 1:length(pa_csv_files)) {
   
 }
 
-# ---- Portuguese ----
-
-# Load PA CSV PT Files list
-pa_csv_files <- list.files("./data/pt/")
 
 
-# Read all CSV PT names and get page creation from Wikipedia
-for (i in 1:length(pa_csv_files)) {
-  print(paste0(i, "in", length(pa_csv_files)))
+# ---- PT PAGE CREATION: Read all CSV ENG names and verify if exists a PT version page ----
+# Then, get info about page creation of the PT version from Wikipedia
+for (i in 1:length(pa_info_eng$codcnuc)) {
+  print(paste(i, "in", length(pa_info_eng$codcnuc)))
   
-  # Split Filename
+  pa_cod_cnuc <- pa_info_eng$codcnuc[i]
+  pa_start_views <- pa_info_eng$pa_start_views[i]
+  pa_end_views <- pa_info_eng$pa_end_views[i]
   
-  pa_filename <- pa_csv_files[i]
+  # Get PA Eng Name from PA Original Dataset
+  pa_name <- pa_original %>%
+    filter(pa_original$codCnuc == pa_cod_cnuc) %>%
+    select(ptNameWiki)
   
-  pa_properties <- unlist(strsplit(pa_filename, "_"))
-  
-  pa_cod_cnuc <- pa_properties[1]
-  pa_start_views <- pa_properties[3]
-  pa_end_views <- unlist(strsplit(pa_properties[4], "[.]"))[1]
+  if (!is.na(pa_name[1,1])) {
+    pa_name <- as.character(pa_name[1,1])
+    
+    # Get date of page creation 
+    url_pa <- paste0("https://pt.wikipedia.org/w/index.php?title=", gsub(" ", "_", pa_name) ,"&action=info")
+    
+    print(pa_name)
+    print(url_pa)
+    
+    pa_page_content <- url_pa %>%
+      read_html() %>%
+      html_node("body #content #bodyContent #mw-content-text .mw-page-info #mw-pageinfo-firsttime") %>%
+      xml_contents() %>%
+      html_text()
+    
+    # Split character variable and standardize date
+    page_creation <- unlist(strsplit(pa_page_content[2], " de "))
+    
+    page_creation_day <- page_creation[2]
+    page_creation_month <- page_creation[3]
+    page_creation_year <- page_creation[4]
+    
+    # Set locale to work with dates
+    Sys.setlocale("LC_TIME", "pt_BR.UTF-8")
+    
+    page_creation <- paste(page_creation_day, page_creation_month, page_creation_year)
+    page_creation <- as.Date(page_creation, tryFormats = c("%d %B %Y"))
+    page_creation <- as.character(page_creation)
+    
+    
+    # Add information to the dataset
+    new_row <- data.table(codcnuc = pa_cod_cnuc, pa_name = pa_name, pa_start_views = pa_start_views, pa_end_views = pa_end_views, page_creation = page_creation)
+    
+    pa_dataset_pt <- rbind(pa_dataset_pt, new_row)
 
-  # Get PA Name from PA Original Dataset
+  }
+}
+
+# ---- PT PAGE CREATION: Read all CSV PT names and get page creation from Wikipedia ----
+for (i in 1:length(pa_info_pt$codcnuc)) {
+  print(paste(i, "in", length(pa_info_pt$codcnuc)))
+  
+  pa_cod_cnuc <- pa_info_pt$codcnuc[i]
+  pa_start_views <- pa_info_pt$pa_start_views[i]
+  pa_end_views <- pa_info_pt$pa_end_views[i]
+  
+  # Get PA Eng Name from PA Original Dataset
   pa_name <- pa_original %>%
     filter(pa_original$codCnuc == pa_cod_cnuc) %>%
     select(ptNameWiki)
   
   pa_name <- as.character(pa_name[1,1])
   
-    
+  
   # Get date of page creation 
   url_pa <- paste0("https://pt.wikipedia.org/w/index.php?title=", gsub(" ", "_", pa_name) ,"&action=info")
   
@@ -137,7 +234,7 @@ for (i in 1:length(pa_csv_files)) {
   page_creation <- as.Date(page_creation, tryFormats = c("%d %B %Y"))
   page_creation <- as.character(page_creation)
   
-    
+  
   # Add information to the dataset
   new_row <- data.table(codcnuc = pa_cod_cnuc, pa_name = pa_name, pa_start_views = pa_start_views, pa_end_views = pa_end_views, page_creation = page_creation)
   
@@ -146,8 +243,11 @@ for (i in 1:length(pa_csv_files)) {
 }
 
 
+
 # Save PA Dataset
 today <- Sys.Date()
 
 write_csv(pa_dataset_eng, paste0("./data/BPA_WikipediaPages_eng_", today, ".csv"))
 write_csv(pa_dataset_pt, paste0("./data/BPA_WikipediaPages_pt_", today, ".csv"))
+
+
