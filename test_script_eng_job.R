8b88d0b6c3085f88de93fba6505ac607a28cde4b
gaiobr <jhonguedes@gmail.com>
2020-09-07 16:53:32 -0300

    Fixed bird diversity problem

--- test_script_eng_job.R
+++ test_script_eng_job.R
@@ -2,15 +2,18 @@
 rm(list=ls())
 
 ####Set working directory
-setwd(choose.dir())
+setwd('F:/GAIO/github/pa-analysis/')
+
+today = Sys.Date()
 
 ####Load data
 #Pa data from EI paper
 #pa_df<-read.table("C:/Users/LACOS21/Documents/tmp_gaio/tmp/data/PA_data_030416.csv",sep=";",dec=".", header=T, quote = "\"")
 #pa_df<-read.table("Z:/Dropbox/Pesquisa/Doutorado/Qualificação II/Análises Ricardo/PA_data_030416_raw.csv",sep=";",dec=".", header=T, quote = "\"")
 #pa_df<-read.table("./data/PA_data_030416.csv",sep=";",dec=".", header=T, quote = "\"")
+pa_df<-read.table("F:/GAIO/data/ricardo/EI/PA_data_030416.csv",sep=";",dec=".", header=T, quote = "\"")
 # Mint
-pa_df<-read.table("/media/gaio/Argos/Dropbox/Pesquisa/Doutorado/Qualificação II/Análises Ricardo/PA_data_030416.csv",sep=";",dec=".", header=T, quote = "\"")
+#pa_df<-read.table("/media/gaio/Argos/Dropbox/Pesquisa/Doutorado/Qualificação II/Análises Ricardo/PA_data_030416.csv",sep=";",dec=".", header=T, quote = "\"")
 print(summary(pa_df))
 
 #Wiki data with PT and EN
@@ -63,9 +66,11 @@ merge_model_df$area_km2_mod<-scale(log10(merge_model_df$area_km2+1))
 merge_model_df$pop_50k_mod<-scale(log10(merge_model_df$pop_50k+1))
 merge_model_df$alt_r_mod<-scale(log10(merge_model_df$alt_r+1))
 merge_model_df$acc_50k_mod<-scale(log10(merge_model_df$acc_50k+1))
-merge_model_df$tot_div_mod<-scale(log10(merge_model_df$tot_div+1))
+merge_model_df$bir_div_mod<-scale(log10(merge_model_df$bir_div+1))
 merge_model_df$Min_dist_mod<-scale(log10(merge_model_df$Min_dist+1))
 merge_model_df$year_mod<-scale(2019-merge_model_df$year)
+
+summary(merge_model_df)
 
 #Lets define the response variable
 merge_model_df$mean.pt<-ifelse(is.na(merge_model_df$mean.pt),0,ceiling(merge_model_df$mean.pt))
@@ -77,24 +82,29 @@ model_df<-merge_model_df[,c(2,5,6,7,8,9:10,14,22,27,31,34,39:45)]
 summary(model_df)
 model_df<-na.exclude(model_df)
 summary(model_df)
-model_df$bioma <- relevel(model_df$bioma, "Caatinga")
+model_df$bioma <- as.factor(model_df$bioma)
+model_df$govern <- as.factor(model_df$govern)
+#model_df$bioma <- relevel(model_df$bioma, "Caatinga")
+#model_df$bioma <- relevel(model_df$bioma, "Cerrado")
+model_df$bioma <- relevel(model_df$bioma, "Amazonia")
 model_df$govern <- relevel(model_df$govern, "federal")
 
-write_csv(model_df, "model_df_hurdle_eng.csv")
+#write_csv(model_df, paste0("model_df_hurdle_eng_cerrado_", today, ".csv"))
+write_csv(model_df, paste0("model_df_hurdle_eng_amazonia_", today, ".csv"))
 
 #And now let's try in EN
-mod1_eng<-hurdle(mean.eng ~ area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div+year_mod+bioma+group+govern|
-                   area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div+year_mod+bioma+group+govern,
+mod1_eng<-hurdle(mean.eng ~ area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div_mod+year_mod+bioma+group+govern|
+                   area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div_mod+year_mod+bioma+group+govern,
              data = model_df,
              dist = "poisson",
              na.action = "na.fail")
-mod2_eng<-hurdle(mean.eng ~ area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div+year_mod+bioma+group+govern|
-                   area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div+year_mod+bioma+group+govern,
+mod2_eng<-hurdle(mean.eng ~ area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div_mod+year_mod+bioma+group+govern|
+                   area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div_mod+year_mod+bioma+group+govern,
              data = model_df,
              dist = "negbin",
              na.action = "na.fail")
-mod3_eng<-hurdle(mean.eng ~ area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div+year_mod+bioma+group+govern|
-                   area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div+year_mod+bioma+group+govern,
+mod3_eng<-hurdle(mean.eng ~ area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div_mod+year_mod+bioma+group+govern|
+                   area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div_mod+year_mod+bioma+group+govern,
              data = model_df,
              dist = "geometric",
              na.action = "na.fail")
@@ -108,18 +118,30 @@ gmod_views_eng
 mod_avg_eng<-model.avg(gmod_views_eng)
 summary(mod_avg_eng)
 
+saveRDS(dd_views_eng, paste0("hurdle_model_ENG_1_Amazonia_", today, ".rds"))
+
 out <- capture.output(summary(mod_avg_eng))
-cat("Hurdle Model ENG", out, file='Hurdle_Model_ENG.txt', sep = '\n', append = FALSE)
+cat("Hurdle Model ENG", out, file=paste0('Hurdle_Model_ENG_Amazonia_', today ,'.txt'), sep = '\n', append = FALSE)
 
 out2 <- capture.output(confint(mod_avg_eng))
-cat("Confint Hurdle Model ENG Avg Model", out2, file='Confint_Hurdle_Avg_Model_ENG.txt', sep = '\n', append = FALSE)
+cat("Confint Hurdle Model ENG Avg Model", out2, file=paste0('Confint_Hurdle_Avg_Model_ENG_Amazonia_', today ,'.txt'), sep = '\n', append = FALSE)
+
+
+# PLOT GRAPH --------------------------------------------------------------
 
 library(ggplot2)
+library(tidyverse)
 library(ggpubr)
 
-model <- read.table("./data/Hurdle Average Confit ENG_count.csv", sep=",", dec=".", header=T)
+today = Sys.Date()
+biome = 'Amazonia'
+#biome = 'Mata-Atlantica'
+
+model <- read.table(paste0("./data/Hurdle Average Confit ENG_", biome, "_2020-09-07.csv"), sep=",", dec=".", header=T) %>% 
+  arrange(desc(order))
+model <- model %>% filter(type == 'count')
 model$var <- factor(model$var, levels=unique(as.character(model$var)) )
-names(model)
+model %>% summary()
 p_eng_count <- ggplot(model, aes(x = var, y = estimate, ymin = inferior, ymax = superior, color = estimate)) +
   geom_pointrange() +
   coord_flip() +
@@ -133,16 +155,18 @@ p_eng_count <- ggplot(model, aes(x = var, y = estimate, ymin = inferior, ymax = 
   theme(legend.position = "none")
 p_eng_count
 
-svg(paste0("./figures/PA_Eng_Count_Model_", Sys.Date(),".svg"))
-#png(paste0("./figures/PA_Eng_Zero_Model", today,".png"))
+svg(paste0("./figures/PA_Eng_Count_Model_", biome, '_', today,".svg"), )
+p_eng_count
+dev.off()  
+png(paste0("./figures/PA_Eng_Count_Model_", biome, '_', today,".png"))
 p_eng_count
 dev.off()  
 
-
-
-model <- read.table("./data/Hurdle Average Confit ENG_zeros.csv", sep=",", dec=".", header=T)
+model <- read.table(paste0("./data/Hurdle Average Confit ENG_", biome, "_2020-09-07.csv"), sep=",", dec=".", header=T) %>% 
+  arrange(desc(order))
+model <- model %>% filter(type == 'zero')
 model$var <- factor(model$var, levels=unique(as.character(model$var)) )
-names(model)
+model %>% summary()
 p_eng_zero <- ggplot(model, aes(x = var, y = estimate, ymin = inferior, ymax = superior, color = estimate)) +
   geom_pointrange() +
   coord_flip() +
@@ -156,8 +180,10 @@ p_eng_zero <- ggplot(model, aes(x = var, y = estimate, ymin = inferior, ymax = s
   theme(legend.position = "none")
 p_eng_zero
 
-svg(paste0("./figures/PA_Eng_Zero_Model_", Sys.Date(),".svg"))
-#png(paste0("./figures/PA_Eng_Zero_Model", today,".png"))
+svg(paste0("./figures/PA_Eng_Zero_Model_", biome, '_', today,".svg"))
+p_eng_zero
+dev.off()  
+png(paste0("./figures/PA_Eng_Zero_Model_", biome, '_', today,".png"))
 p_eng_zero
 dev.off()  
 
--- test_script_pt_job.R
+++ test_script_pt_job.R
@@ -2,14 +2,17 @@
 rm(list=ls())
 
 ####Set working directory
-setwd(choose.dir())
+setwd('F:/GAIO/github/pa-analysis/')
+
+today = Sys.Date()
 
 ####Load data
 #Pa data from EI paper
 # Lacos
-pa_df<-read.table("C:/Users/LACOS21/Documents/tmp_gaio/tmp/data/PA_data_030416.csv",sep=";",dec=".", header=T, quote = "\"")
+#pa_df<-read.table("C:/Users/LACOS21/Documents/tmp_gaio/tmp/data/PA_data_030416.csv",sep=";",dec=".", header=T, quote = "\"")
 #pa_df<-read.table("Z:/Dropbox/Pesquisa/Doutorado/Qualificação II/Análises Ricardo/PA_data_030416_raw.csv",sep=";",dec=".", header=T, quote = "\"")
 #pa_df<-read.table("./data/PA_data_030416.csv",sep=";",dec=".", header=T, quote = "\"")
+pa_df<-read.table("F:/GAIO/data/ricardo/EI/PA_data_030416.csv",sep=";",dec=".", header=T, quote = "\"")
 # Mint
 #pa_df<-read.table("/media/gaio/Argos/Dropbox/Pesquisa/Doutorado/Qualificação II/Análises Ricardo/PA_data_030416.csv",sep=";",dec=".", header=T, quote = "\"")
 print(summary(pa_df))
@@ -64,7 +67,7 @@ merge_model_df$area_km2_mod<-scale(log10(merge_model_df$area_km2+1))
 merge_model_df$pop_50k_mod<-scale(log10(merge_model_df$pop_50k+1))
 merge_model_df$alt_r_mod<-scale(log10(merge_model_df$alt_r+1))
 merge_model_df$acc_50k_mod<-scale(log10(merge_model_df$acc_50k+1))
-merge_model_df$tot_div_mod<-scale(log10(merge_model_df$tot_div+1))
+merge_model_df$bir_div_mod<-scale(log10(merge_model_df$bir_div+1))
 merge_model_df$Min_dist_mod<-scale(log10(merge_model_df$Min_dist+1))
 merge_model_df$year_mod<-scale(2019-merge_model_df$year)
 
@@ -78,24 +81,30 @@ model_df<-merge_model_df[,c(2,5,6,7,8,9:10,14,22,27,31,34,39:45)]
 summary(model_df)
 model_df<-na.exclude(model_df)
 summary(model_df)
-model_df$bioma <- relevel(model_df$bioma, "Caatinga")
+model_df$bioma <- as.factor(model_df$bioma)
+model_df$govern <- as.factor(model_df$govern)
+#model_df$bioma <- relevel(model_df$bioma, "Caatinga")
+#model_df$bioma <- relevel(model_df$bioma, "Cerrado")
+#model_df$bioma <- relevel(model_df$bioma, "Mata Atlantica")
+model_df$bioma <- relevel(model_df$bioma, "Amazonia")
 model_df$govern <- relevel(model_df$govern, "federal")
 
-write_csv(model_df, "model_df_hurdle_pt.csv")
+#write_csv(model_df, paste0("model_df_hurdle_eng_cerrado_", today, ".csv"))
+write_csv(model_df, paste0("model_df_hurdle_eng_amazonia_", today, ".csv"))
 
 #And now let's try in PT
-mod1_pt<-hurdle(mean.pt ~ area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div+year_mod+bioma+group+govern|
-                   area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div+year_mod+bioma+group+govern,
+mod1_pt = hurdle(mean.pt ~ area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div_mod+year_mod+bioma+group+govern|
+                   area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div_mod+year_mod+bioma+group+govern,
              data = model_df,
              dist = "poisson",
              na.action = "na.fail")
-mod2_pt<-hurdle(mean.pt ~ area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div+year_mod+bioma+group+govern|
-                   area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div+year_mod+bioma+group+govern,
+mod2_pt<-hurdle(mean.pt ~ area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div_mod+year_mod+bioma+group+govern|
+                   area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div_mod+year_mod+bioma+group+govern,
              data = model_df,
              dist = "negbin",
              na.action = "na.fail")
-mod3_pt<-hurdle(mean.pt ~ area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div+year_mod+bioma+group+govern|
-                   area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div+year_mod+bioma+group+govern,
+mod3_pt<-hurdle(mean.pt ~ area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div_mod+year_mod+bioma+group+govern|
+                   area_km2_mod+pop_50k_mod+alt_r_mod+acc_50k_mod+bir_div_mod+year_mod+bioma+group+govern,
              data = model_df,
              dist = "geometric",
              na.action = "na.fail")
@@ -103,6 +112,7 @@ AICc(mod1_pt,mod2_pt,mod3_pt)
 
 #Negative binomial seems to be the best choice based on AICc - Let's keep it and run all model combinations
 dd_views_pt <- dredge(mod2_pt,evaluate=TRUE, rank = "AICc", REML = F)
+
 #dd_views_pt <- readRDS("hurdle_model_ENG_1.1.rds")
 dd_views_pt
 gmod_views_pt <- get.models(dd_views_pt,subset = delta < 4)
@@ -110,21 +120,32 @@ gmod_views_pt
 mod_avg_pt<-model.avg(gmod_views_pt)
 summary(mod_avg_pt)
 
-saveRDS(dd_views_pt, "hurdle_model_PT_1.rds")
+saveRDS(dd_views_pt, paste0("hurdle_model_PT_1_Amazonia_", today, ".rds"))
 
 out <- capture.output(summary(mod_avg_pt))
-cat("Hurdle Model PT", out, file='Hurdle_Model_PT.txt', sep = '\n', append = FALSE)
+cat("Hurdle Model PT", out, file=paste0('Hurdle_Model_PT_Amazonia_', today ,'.txt'), sep = '\n', append = FALSE)
 
 out2 <- capture.output(confint(mod_avg_pt))
-cat("Confint Hurdle Model PT Avg Model", out2, file='Confint_Hurdle_Avg_Model_PT.txt', sep = '\n', append = FALSE)
-
+cat("Confint Hurdle Model PT Avg Model", out2, file=paste0('Confint_Hurdle_Avg_Model_PT_Amazonia_' , today ,'.txt'), sep = '\n', append = FALSE)
 ###calc.relimp(dd_views_pt, type = c("lmg"), rela = TRUE) 
 
-library(ggplot2)
 
-model <- read.table("./data/Hurdle Average Confit PT_count.csv", sep=",", dec=".", header=T)
+# PLOT GRAPH --------------------------------------------------------------
+
+
+
+library(ggplot2)
+library(tidyverse)
+
+today = Sys.Date()
+biome = 'Amazonia'
+#biome = 'Mata-Atlantica'
+
+model <- read.table(paste0('./data/Hurdle Average Confit PT_', biome ,'_2020-09-07.csv'), sep=",", dec=".", header=T) %>% 
+  arrange(desc(order))
+model <- model %>% filter(type == 'count')
 model$var <- factor(model$var, levels=unique(as.character(model$var)) )
-names(model)
+model %>% summary()
 p_pt_count <- ggplot(model, aes(x = var, y = estimate, ymin = inferior, ymax = superior, color = estimate)) +
   geom_pointrange() +
   coord_flip() +
@@ -138,14 +159,17 @@ p_pt_count <- ggplot(model, aes(x = var, y = estimate, ymin = inferior, ymax = s
   theme(legend.position = "none")
 p_pt_count
 
-svg(paste0("./figures/PA_Pt_Count_Model_", Sys.Date(),".svg"))
-#png(paste0("./figures/PA_Eng_Zero_Model", today,".png"))
+svg(paste0("./figures/PA_PT_Count_Model_", biome, '_', today,".svg"))
 p_pt_count
-dev.off()  
+dev.off()
+png(paste0("./figures/PA_PT_Count_Model_", biome, '_', today,".png"))
+p_pt_count
+dev.off()
 
 
-model <- read.table("./data/Hurdle Average Confit PT_zeros.csv", sep=",", dec=".", header=T)
-model2 <- read.table("./data/Hurdle Average Confit ENG_zeros.csv", sep=",", dec=".", header = T)
+model <- read.table(paste0("./data/Hurdle Average Confit PT_", biome ,"_2020-09-07.csv"), sep=",", dec=".", header=T) %>% 
+  arrange(desc(order))
+model <- model %>% filter(type == 'zero')
 model$var <- factor(model$var, levels=unique(as.character(model$var)) )
 names(model)
 p_pt_zero <- ggplot(model, aes(x = var, y = estimate, ymin = inferior, ymax = superior, color = estimate)) +
@@ -161,7 +185,9 @@ p_pt_zero <- ggplot(model, aes(x = var, y = estimate, ymin = inferior, ymax = su
   theme(legend.position = "none")
 p_pt_zero
 
-svg(paste0("./figures/PA_Pt_Zero_Model_", Sys.Date(),".svg"))
-#png(paste0("./figures/PA_Eng_Zero_Model", today,".png"))
+svg(paste0("./figures/PA_PT_Zero_Model_", biome, '_', today,".svg"))
 p_pt_zero
-dev.off()  
+dev.off()
+png(paste0("./figures/PA_PT_Zero_Model_", biome, '_', today,".png"))
+p_pt_zero
+dev.off()
